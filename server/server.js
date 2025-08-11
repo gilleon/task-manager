16fe028e795ddd1c1b8c670ff5f94fb73c5836da
airlorm <elormnizi@gmail.com>
2025-08-11 22:20:24 +0300

    refactor: comment out database connection and organize task routes

--- server/server.js
+++ server/server.js
@@ -8,10 +8,15 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-connectDB();
+//connectDB();
 
 app.use(cors());
 app.use(express.json());
+
+const taskRoutes = require('./routes/tasks');
+
+
+app.use('/tasks', taskRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
